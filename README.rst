5c253f6a06e642a4b5069eb50b6f8eab2d0cba8d
Kowies <kowieski.m98@gmail.com>
2019-10-29 22:40:14 +0100

    Create default file structure.

---
+++ README.rst
@@ -0,0 +1,1 @@
+Tu będą podstawowe informacje.
--- code/hashtable.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-
-parse = argparse.ArgumentParser()
-
-parse.add_argument('--data', help='ścieżka do pliku z tekstem')
-args = parse.parse_args()
