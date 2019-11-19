3062c5d2872358eff45792c1d1a6a9b8ae035c50
Kowies <kowieski.m98@gmail.com>
2019-11-19 22:43:46 +0100

    Add test_hashtable_discard

--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -10,3 +10,19 @@ def test_hashtable_add():
     assert string not in hashtable 
     hashtable.add(string)
     assert string in hashtable
+
+
+def test_hashtable_discard():
+    hashtable = Hashtable()
+    string = 'AbCdEfG'
+
+    assert string not in hashtable 
+    hashtable.discard(string)
+    assert string not in hashtable
+
+    hashtable.add(string)
+    assert string in hashtable
+    hashtable.discard(string)
+    assert string not in hashtable
+    hashtable.discard(string)
+    assert string not in hashtable
