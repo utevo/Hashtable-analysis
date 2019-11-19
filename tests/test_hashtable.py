4eb59cc4504a31bc2b0cd87475bbc8af14967579
Kowies <kowieski.m98@gmail.com>
2019-11-19 22:48:13 +0100

    Create test_hashtable_len

--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -26,3 +26,25 @@ def test_hashtable_discard():
     assert string not in hashtable
     hashtable.discard(string)
     assert string not in hashtable
+
+
+def test_hashtable_len():
+    hashtable = Hashtable()
+    string_1 = 'Ambroży'
+    string_2 = 'Kleks'
+
+    assert len(hashtable) == 0
+    hashtable.add(string_1)
+    assert len(hashtable) == 1
+    hashtable.add(string_2)
+    assert len(hashtable) == 2
+
+    hashtable.discard(string_1)
+    assert len(hashtable) == 1
+    hashtable.discard(string_1)
+    assert len(hashtable) == 1
+
+    hashtable.discard(string_2)
+    assert len(hashtable) == 0
+    hashtable.discard(string_2)
+    assert len(hashtable) == 0
