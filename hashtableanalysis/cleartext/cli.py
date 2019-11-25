8832650433eb4084ee1d155f13cfc460aa50e60c
Kowies <kowieski.m98@gmail.com>
2019-11-25 23:10:20 +0100

    Update cleartext/cli.py

--- hashtableanalysis/cleartext/cli.py
+++ hashtableanalysis/cleartext/cli.py
@@ -7,12 +7,25 @@ from . import __version__
 
 
 @click.command(name='cleartext')
+@click.argument('input', type=click.File('r'))
+@click.argument('output', type=click.File('w'))
 @click.version_option(version=__version__)
-def main():
-    input = sys.stdin.read()
-    output = cleartext(input)
-    click.echo(output)
+def cli(input, output):
+    """Pobiera słowa z INPUT oczyszcza je i zwraca
+    do OUTPUT.
+
+    \b
+    Czyści słowa z stdin i zwraca je do stdout:
+        cleartext - -
+    
+    \b
+    Czyści słowa z pliku foo.txt i zwraca je do pliku bar.txt:
+        cleartext foo.txt bar.txt
+    """
+    dirty_text = input.read()
+    words = cleartext(dirty_text)
+    output.write(str(words))
 
 
 if __name__ == '__main__':
-    main()
+    cli()
