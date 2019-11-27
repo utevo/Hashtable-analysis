59df4509f8d6fd78af69ebd6fa4d4dd9218e7d53
Kowies <kowieski.m98@gmail.com>
2019-11-27 20:10:25 +0100

    Create code for benchmark method (hashtable/cli.py module)

--- hashtableanalysis/hashtable/cli.py
+++ hashtableanalysis/hashtable/cli.py
@@ -1,3 +1,6 @@
+import time
+import statistics
+
 import click
 
 from hashtableanalysis.hashtable.hashtable import (
@@ -5,6 +8,13 @@ from hashtableanalysis.hashtable.hashtable import (
 from hashtableanalysis.cleartext.cleartext import cleartext
 from hashtableanalysis.wordgenerator.wordgenerator import wordgenerator
 from . import __version__
+
+
+def add_n_elemnts_from_word_generator_to_hashtable(number_of_words,
+                                                   wordgenerator, hashtable):
+    for __ in range(number_of_words):
+        word = next(wordgenerator)
+        hashtable.add(word)
 
 
 @click.group()
@@ -51,9 +61,8 @@ def generate(input, output, number_of_words):
     word_generator = wordgenerator(words)
     hashtable = Hashtable()
 
-    for __ in range(number_of_words):
-        new_word = next(word_generator)
-        hashtable.add(new_word)
+    add_n_elemnts_from_word_generator_to_hashtable(number_of_words,
+                                                   word_generator, hashtable)
 
     viewer = HashtableViewer(hashtable)
     df = viewer.low_level()
@@ -80,9 +89,59 @@ def benchmark(input, output, initial_number_of_words, step,
         number_of_problems (int): Ilość rozwiązywanych problemów.
         number_of_insances (int): Ilość instancji problemu.
     """
-    pass
+    text = input.read()
+    words = cleartext(text)
+    word_generator = wordgenerator(words)
+    
+    number_of_words = initial_number_of_words
+    mean_exec_time_for_problems = []
+    for __ in range(number_of_problems):
+
+        exec_times_for_problem = []
+        for __ in range(number_of_instances):
+            hashtable = Hashtable(10000)
+
+            start_time = time.time()
+            add_n_elemnts_from_word_generator_to_hashtable(number_of_words,
+                                                           word_generator, hashtable)
+            end_time = time.time()
+
+            exec_time_for_instance = end_time - start_time
+            exec_times_for_problem.append(exec_time_for_instance)
+            
+        mean_exec_time_for_problem = statistics.mean(exec_times_for_problem)
+        mean_exec_time_for_problems.append(mean_exec_time_for_problem)
+        number_of_words += step
+    
+    n_values = []
+    number_of_words = initial_number_of_words
+    for __ in range(number_of_problems):
+        n_values.append(number_of_words)
+        number_of_words += step
+
+    # t_t(n) = O(n)
+    median_index = number_of_problems // 2
+    median_of_n_values = n_values[median_index]
+    mean_exec_time_for_median = mean_exec_time_for_problems[median_index]
+    # 1 = mean_exec_time_for_median / C * median_of_n_valus
+    #       ||
+    #       \/
+    # C = mean_exec_time_for_median / median_of_n_valus
+    C = mean_exec_time_for_median / median_of_n_values
 
 
+    t_values = mean_exec_time_for_problems
+
+    q_values = [None] * number_of_problems
+    for index in range(number_of_problems):
+        q_values[index] = t_values[index] / (C * n_values[index])
+
+
+    print('n_values', n_values)
+    print('t_values', t_values)
+    print('q_values', q_values)
+
+    
 cli.add_command(io)
 cli.add_command(generate)
 cli.add_command(benchmark)
